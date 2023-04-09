7219be7e55214d884dfbed075a898d13dba456ce
Om Uskaikar <117098130+omuskaikar@users.noreply.github.com>
2023-04-09 11:55:35 +0530

    Update Exception_handling.py

--- Exception_handling.py
+++ Exception_handling.py
@@ -21,8 +21,6 @@ def counter(fname):
     print("Number of lines in text file: ", num_lines)
     print("Number of characters in text file: ", num_charc)
     print("Number of spaces in text file: ", num_spaces)
-
-
 if __name__ == "__main__":
     fname = "om.txt"
     try:
@@ -30,23 +28,26 @@ if __name__ == "__main__":
     except:
         print("File not found")
 
+        
+        
 
 class Invalid_Marks(Exception):
     def __str__(self):
         return "the marks entered are above 100"
-
-
 marks = int(input("enter the marks:"))
 if marks > 100:
     raise Invalid_Marks
 else:
     print("valid marks")
 
+    
+    
+    
+    
 
 class zeroerr(Exception):
     def __str__(self):
         return "the value of b*d is 0"
-
 
 a = int(input("enter value for a:"))
 b = int(input("enter value for b:"))
@@ -59,20 +60,23 @@ else:
     print(f"the value of given expression is:{value}")
 
 
+    
+    
+    
 age = int(input("enter the age:"))
-
-
 class chotu(Exception):
     def __str__(self):
         return "chotu hai tu"
-
-
 if age < 18:
     raise chotu
 else:
     print("valid age")
 
 
+    
+    
+    
+    
 if __name__ == "__main__":
     fname = "File1.txt"
     try:
@@ -80,12 +84,14 @@ if __name__ == "__main__":
     except:
         print("File not found")
 
+        
+        
+        
+        
 
 class TypeError(Exception):
     def __str__(self):
         return "wrong datatype"
-
-
 a = 15
 b = "om"
 if type(b) != str:
